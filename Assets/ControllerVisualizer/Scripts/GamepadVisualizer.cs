8b085a6c39a3ea4fcd6bfbc9ed57d66cc574dd73
RedSPINE <rc.redspine@gmail.com>
2020-11-25 16:02:36 +0100

    v2

--- Assets/ControllerVisualizer/Scripts/GamepadVisualizer.cs
+++ Assets/ControllerVisualizer/Scripts/GamepadVisualizer.cs
@@ -5,6 +5,8 @@ namespace ControllerVisualizer
 {
     public class GamepadVisualizer : MonoBehaviour
     {
+        [SerializeField] private GameObject unboundCache = default;
+
         public int controlIndex
         {
             get => m_ControlIndex;
@@ -18,12 +20,19 @@ namespace ControllerVisualizer
 
         private void Awake() {
             ResolveControl();
+            InputSystem.onDeviceChange += (device, change) => ResolveControl();
         }
 
         private void ResolveControl()
         {
             InputVisualizer[] visualizers = GetComponentsInChildren<InputVisualizer>();
-            var deviceId = Gamepad.all[m_ControlIndex].deviceId;
+            var gamepads = Gamepad.all;
+            if (gamepads.Count <= this.controlIndex) {
+                unboundCache.SetActive(true);
+                throw new System.Exception("Not enough gamepads detected. Please connect more gamepads.");
+            }
+            var deviceId = gamepads[m_ControlIndex].deviceId;
+            unboundCache.SetActive(false);
             foreach (InputVisualizer visualizer in visualizers)
             {
                 visualizer.deviceId = deviceId;
